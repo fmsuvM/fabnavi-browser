002671b36eb2311e2bfbe0af983890c2536479c6
fmsuvM <k222.tkd.fms@gmail.com>
2018-10-25 16:37:46 +0900

    delete unneccesarry config style

--- src/renderer/stylesheets/GlobalStyle.js
+++ src/renderer/stylesheets/GlobalStyle.js
@@ -1,21 +1,6 @@
 import { injectGlobal } from 'styled-components';
-import NotoSansR from '../fonts/NotoSans-Regular.ttf';
-import NotoSansB from '../fonts/NotoSans-Bold.ttf';
-import ArialRoundedMTBoldBold from '../fonts/ArialRoundedMTBoldBold.ttf';
 
 injectGlobal`
-    @font-face {
-        font-family : "ArialRoundedMTBoldBold";
-        src: url(${ArialRoundedMTBoldBold});
-    }
-    @font-face {
-        font-family : "NotoSans-Bold";
-        src: url(${NotoSansB});
-    }
-    @font-face {
-        font-family : "NotoSans-Regular";
-        src: url(${NotoSansR});
-    }
     body {
         font-family: Helvetica, Arial, '游ゴシック', 'Yu Gothic', '游ゴシック体', YuGothic, sans-serif;
         margin: 0;
