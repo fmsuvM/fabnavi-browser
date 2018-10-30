e2cf8ccf196060e959b94ff39bbdb16f570e798b
fmsuvM <k222.tkd.fms@gmail.com>
2018-10-30 13:44:37 +0900

    [WIP]: fix login

--- src/renderer/components/MenuIcon.js
+++ src/renderer/components/MenuIcon.js
@@ -1,143 +1,192 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import Debug from "debug";
-import { push } from "react-router-redux";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Debug from 'debug';
+import { push } from 'react-router-redux';
 
-import { host } from "../utils/host";
-import api from "../utils/WebAPIUtils";
+import { host } from '../utils/host';
+import api from '../utils/WebAPIUtils';
 
-import { signInFailed, signedIn, signedOut, signingOut } from "../actions/users";
+import { signInFailed, signedIn, signedOut, signingOut } from '../actions/users';
 
-import { IconStyle, LogoStyle } from "../stylesheets/application/ProjectIndex/StyledMenuIcon";
-import { Button, SignOutButton } from "../stylesheets/application/interface/StyledButton";
-import { changeProjectListPage } from "../actions/manager";
+import { IconStyle, LogoStyle } from '../stylesheets/application/ProjectIndex/StyledMenuIcon';
+import { Button, SignOutButton } from '../stylesheets/application/interface/StyledButton';
+import { changeProjectListPage } from '../actions/manager';
 
-const debug = Debug("fabnavi:jsx:MenuIcon");
+const debug = Debug('fabnavi:jsx:MenuIcon');
 
 // TODO: split class
-export const MenuIcon = props => {
-  const signIn = () => {
-    debug("sign in process is starting");
-    // const authUrl = `${host.url}/auth/github?auth_origin_url=${host.url}`;
-    // const authWindow = new remote.BrowserWindow({
-    //     modal: true,
-    //     width: 400,
-    //     height: 800,
-    //     webPreferences: {
-    //         webSecurity: false
-    //     }
-    // });
-    // authWindow.loadURL(authUrl);
-    // const onMessage = () => {
-    //     debug(authWindow.getURL());
-    //     const url = authWindow.getURL();
-    //     if(url.includes('uid') && url.includes('client_id') && url.includes('auth_token')) {
-    //         props.signedIn({
-    //             'Access-Token': url.match(/auth_token=([a-zA-Z0-9\-_]*)/)[1],
-    //             Uid: url.match(/uid=([a-zA-Z0-9\-_]*)/)[1],
-    //             Client: url.match(/client_id=([a-zA-Z0-9\-_]*)/)[1]
-    //         });
-    //         authWindow.close();
-    //     }
-    // };
-    // authWindow.once('message', onMessage);
-    // authWindow.on('page-title-updated', onMessage);
-  };
+export class MenuIcon extends React.Component {
+    constructor(props) {
+        super(props);
 
-  const _onClick = () => {
-    if (props.hasOwnProperty("to")) {
-      if (props.to === "/" && props.currentPage !== 0) {
-        props.jumpToHome();
-      }
-      props.jump(props.to);
+        this.signIn = () => {
+            debug('sign in process is starting');
+            // const onMessage = e => {
+            //     debug('e: ', e.data);
+            // };
+            // window.addEventListener('message', onMessage);
+            // const testurl = 'http://example.com/';
+            // window.open(testurl, 'test');
+            const _url = window.location.href;
+            const authUrl = `${host.url}/auth/github?auth_origin_url=${_url}`;
+            window.open(authUrl, 'GitHub Login');
+            debug('href uri?: ', window.location.href);
+            const onMessage = e => {
+                debug('onmessage: ', e);
+                window.removeEventListener('message', onMessage, false);
+                const url = window.location.href;
+                if(url.includes('uid') && url.includes('client_id') && url.includes('auth_token')) {
+                    debug('hoge');
+                    props.signedIn({
+                        'Access-Token': authUrl.match(/auth_token=([a-zA-Z0-9\-_]*)/)[1],
+                        Uid: authUrl.match(/uid=([a-zA-Z0-9\-_]*)/)[1],
+                        Client: authUrl.match(/client_id=([a-zA-Z0-9\-_]*)/)[1]
+                    });
+                }
+            };
+            window.addEventListener('message', onMessage); // 親window
+        };
+
+        this._onClick = () => {
+            if(props.hasOwnProperty('to')) {
+                if(props.to === '/' && props.currentPage !== 0) {
+                    props.jumpToHome();
+                }
+                props.jump(props.to);
+            }
+            if(props.hasOwnProperty('act')) {
+                if(props.act === 'sign_in') {
+                    debug('sign in action');
+                    this.signIn();
+                } else {
+                    props.signingOut();
+                    api.signOut()
+                        .then(res => {
+                            debug('response is: ', res);
+                            props.signedOut();
+                        })
+                        .catch(err => debug('error is occuered: ', err));
+                }
+            }
+        };
     }
-    if (props.hasOwnProperty("act")) {
-      if (props.act === "sign_in") {
-        debug("sign in action");
-        signIn();
-      } else {
-        props.signingOut();
-        api
-          .signOut()
-          .then(res => {
-            debug("response is: ", res);
-            props.signedOut();
-          })
-          .catch(err => debug("error is occuered: ", err));
-      }
+
+    // onMessage(e) {
+    //     debug('event: ', e);
+    // }
+
+    // componentDidMount() {
+    //     window.opener.postMessage('waiwai', '*')
+    //     window.addEventListener('message', this.onMessage);
+    // }
+
+    render() {
+        return (
+            <div>
+                <a onClick={this._onClick}>
+                    {this.props.act === 'sign_in' ? (
+                        <Button>Sign In</Button>
+                    ) : this.props.act === 'sign_out' ? (
+                        <SignOutButton>Sign Out</SignOutButton>
+                    ) : this.props.help === true ? (
+                        <IconStyle help src={this.props.src} />
+                    ) : this.props.logo === true ? (
+                        <LogoStyle type="logo" src={this.props.src} />
+                    ) : (
+                        <IconStyle src={this.props.src} />
+                    )}
+                </a>
+            </div>
+        );
     }
-  };
-  return (
-    <div>
-      <a onClick={_onClick}>
-        {props.act === "sign_in" ? (
-          <Button>Sign In</Button>
-        ) : props.act === "sign_out" ? (
-          <SignOutButton>Sign Out</SignOutButton>
-        ) : props.help === true ? (
-          <IconStyle help src={props.src} />
-        ) : props.logo === true ? (
-          <LogoStyle type="logo" src={props.src} />
-        ) : (
-          <IconStyle src={props.src} />
-        )}
-      </a>
-    </div>
-  );
-};
+}
+// export const _MenuIcon = props => {
+//     const signIn = () => {
+//         debug('sign in process is starting');
+//         const testurl = 'http://example.com/';
+//         window.open(testurl, 'test');
+//         const onMessage = e => {
+//             debug('e: ', e.data);
+//         };
+//         document.addEventListener('message', onMessage);
+//         // const authUrl = `${host.url}/auth/github?auth_origin_url=${host.url}`;
+//         // window.open(authUrl, 'GitHub Login');
+//         // const onMessage = e => {
+//         //     debug('onmessage');
+//         //     window.removeEventListener('message', onMessage, false);
+//         //     const url = window.location.href;
+//         //     if(url.includes('uid') && url.includes('client_id') && url.includes('auth_token')) {
+//         //         debug('hoge');
+//         //         props.signedIn({
+//         //             'Access-Token': authUrl.match(/auth_token=([a-zA-Z0-9\-_]*)/)[1],
+//         //             Uid: authUrl.match(/uid=([a-zA-Z0-9\-_]*)/)[1],
+//         //             Client: authUrl.match(/client_id=([a-zA-Z0-9\-_]*)/)[1]
+//         //         });
+//         //     }
+//         // };
+//         // window.addEventListener('message', onMessage); // 親window
+//     };
+
+//     const _onClick = () => {
+
+//     };
+//     return (
+
+//     );
+// };
 
 MenuIcon.propTypes = {
-  jump: PropTypes.func,
-  signedIn: PropTypes.func,
-  signedOut: PropTypes.func,
-  signingOut: PropTypes.func,
-  src: PropTypes.string,
-  to: PropTypes.string,
-  act: PropTypes.string,
-  currentPage: PropTypes.number,
-  jumpToHome: PropTypes.func
+    jump: PropTypes.func,
+    signedIn: PropTypes.func,
+    signedOut: PropTypes.func,
+    signingOut: PropTypes.func,
+    src: PropTypes.string,
+    to: PropTypes.string,
+    act: PropTypes.string,
+    currentPage: PropTypes.number,
+    jumpToHome: PropTypes.func
 };
 
 export const mapStateToProps = state => ({
-  currentPage: state.manager.currentPage
+    currentPage: state.manager.currentPage
 });
 
 export const mapDispatchToProps = dispatch => ({
-  jumpToHome: () => {
-    dispatch(changeProjectListPage(0));
-  },
-  jump: path => {
-    dispatch(push(path));
-  },
-  signedIn: credential => {
-    debug("credential: ", credential);
-    api.saveCredential(credential);
-    api.prepareHeaders();
-    dispatch(signedIn(credential));
-  },
-  signingOut: () => {
-    dispatch(signingOut());
-  },
-  signedOut: () => {
-    api.clearCredential();
-    api.clearUserId();
-    dispatch(signedOut());
-  },
-  signInFailed: (error, info) => {
-    const now = new Date();
-    dispatch(
-      signInFailed({
-        message: "sign in failed. see console",
-        error,
-        info,
-        time: now.toTimeString()
-      })
-    );
-  }
+    jumpToHome: () => {
+        dispatch(changeProjectListPage(0));
+    },
+    jump: path => {
+        dispatch(push(path));
+    },
+    signedIn: credential => {
+        debug('credential: ', credential);
+        api.saveCredential(credential);
+        api.prepareHeaders();
+        dispatch(signedIn(credential));
+    },
+    signingOut: () => {
+        dispatch(signingOut());
+    },
+    signedOut: () => {
+        api.clearCredential();
+        api.clearUserId();
+        dispatch(signedOut());
+    },
+    signInFailed: (error, info) => {
+        const now = new Date();
+        dispatch(
+            signInFailed({
+                message: 'sign in failed. see console',
+                error,
+                info,
+                time: now.toTimeString()
+            })
+        );
+    }
 });
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(MenuIcon);
