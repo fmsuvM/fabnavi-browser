02332a392c8f91ee57db5972d7cc5d2dff452ba0
fmsuvM <k222.tkd.fms@gmail.com>
2018-11-04 23:13:34 +0900

    fix sign in process

--- src/renderer/components/MenuIcon.js
+++ src/renderer/components/MenuIcon.js
@@ -17,125 +17,126 @@ const debug = Debug('fabnavi:jsx:MenuIcon');
 
 // TODO: split class
 export class MenuIcon extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.signIn = () => {
-            debug('sign in process is starting');
-            const authUrl = `${host.url}/auth/github?auth_origin_url=${window.location.href}`;
-            window.open(authUrl, 'GitHub Login');
-            const onMessage = e => {
-                debug('onmessage: ', e);
-                window.removeEventListener('message', onMessage, false);
-                if(e.origin == window.location.origin) {
-                    try{
-                        debug('>>', e.data);
-                        this.props.signedIn(JSON.parse(e.data));
-                    } catch(error) {
-                        this.props.signInFailed(error, e);
-                    }
-                }
-            };
-            window.addEventListener('message', onMessage);
-        };
+    this.signIn = () => {
+      debug('sign in process is starting');
+      const authUrl = `${host.url}/auth/github?auth_origin_url=${window.location.href}`;
+      window.open(authUrl, 'GitHub Login');
+      const onMessage = e => {
+        debug('onmessage: ', e);
+        window.removeEventListener('message', onMessage, false);
+        if(e.origin == window.location.origin) {
+          try{
+            debug('>>', e.data);
+            this.props.signedIn(JSON.parse(e.data));
+          } catch(error) {
+            this.props.signInFailed(error, e);
+          }
+        }
+      };
+      window.addEventListener('message', onMessage);
+    };
 
-        this._onClick = () => {
-            if(props.hasOwnProperty('to')) {
-                if(props.to === '/' && props.currentPage !== 0) {
-                    props.jumpToHome();
-                }
-                props.jump(props.to);
-            }
-            if(props.hasOwnProperty('act')) {
-                if(props.act === 'sign_in') {
-                    debug('sign in action');
-                    this.signIn();
-                } else {
-                    props.signingOut();
-                    api.signOut()
-                        .then(res => {
-                            debug('response is: ', res);
-                            props.signedOut();
-                        })
-                        .catch(err => debug('error is occuered: ', err));
-                }
-            }
-        };
-    }
+    this._onClick = () => {
+      if(props.hasOwnProperty('to')) {
+        if(props.to === '/' && props.currentPage !== 0) {
+          props.jumpToHome();
+        }
+        props.jump(props.to);
+      }
+      if(props.hasOwnProperty('act')) {
+        if(props.act === 'sign_in') {
+          debug('sign in action');
+          this.signIn();
+        } else {
+          props.signingOut();
+          api
+            .signOut()
+            .then(res => {
+              debug('response is: ', res);
+              props.signedOut();
+            })
+            .catch(err => debug('error is occuered: ', err));
+        }
+      }
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <a onClick={this._onClick}>
-                    {this.props.act === 'sign_in' ? (
-                        <Button>Sign In</Button>
-                    ) : this.props.act === 'sign_out' ? (
-                        <SignOutButton>Sign Out</SignOutButton>
-                    ) : this.props.help === true ? (
-                        <IconStyle help src={this.props.src} />
-                    ) : this.props.logo === true ? (
-                        <LogoStyle type="logo" src={this.props.src} />
-                    ) : (
-                        <IconStyle src={this.props.src} />
-                    )}
-                </a>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <a onClick={this._onClick}>
+          {this.props.act === 'sign_in' ? (
+            <Button>Sign In</Button>
+          ) : this.props.act === 'sign_out' ? (
+            <SignOutButton>Sign Out</SignOutButton>
+          ) : this.props.help === true ? (
+            <IconStyle help src={this.props.src} />
+          ) : this.props.logo === true ? (
+            <LogoStyle type="logo" src={this.props.src} />
+          ) : (
+            <IconStyle src={this.props.src} />
+          )}
+        </a>
+      </div>
+    );
+  }
 }
 
 MenuIcon.propTypes = {
-    jump: PropTypes.func,
-    signedIn: PropTypes.func,
-    signInFailed: PropTypes.func,
-    signedOut: PropTypes.func,
-    signingOut: PropTypes.func,
-    src: PropTypes.string,
-    to: PropTypes.string,
-    act: PropTypes.string,
-    currentPage: PropTypes.number,
-    jumpToHome: PropTypes.func
+  jump: PropTypes.func,
+  signedIn: PropTypes.func,
+  signInFailed: PropTypes.func,
+  signedOut: PropTypes.func,
+  signingOut: PropTypes.func,
+  src: PropTypes.string,
+  to: PropTypes.string,
+  act: PropTypes.string,
+  currentPage: PropTypes.number,
+  jumpToHome: PropTypes.func
 };
 
 export const mapStateToProps = state => ({
-    currentPage: state.manager.currentPage
+  currentPage: state.manager.currentPage
 });
 
 export const mapDispatchToProps = dispatch => ({
-    jumpToHome: () => {
-        dispatch(changeProjectListPage(0));
-    },
-    jump: path => {
-        dispatch(push(path));
-    },
-    signedIn: credential => {
-        debug('credential: ', credential);
-        api.saveCredential(credential);
-        api.prepareHeaders();
-        dispatch(signedIn(credential));
-    },
-    signingOut: () => {
-        dispatch(signingOut());
-    },
-    signedOut: () => {
-        api.clearCredential();
-        api.clearUserId();
-        dispatch(signedOut());
-    },
-    signInFailed: (error, info) => {
-        const now = new Date();
-        dispatch(
-            signInFailed({
-                message: 'sign in failed. see console',
-                error,
-                info,
-                time: now.toTimeString()
-            })
-        );
-    }
+  jumpToHome: () => {
+    dispatch(changeProjectListPage(0));
+  },
+  jump: path => {
+    dispatch(push(path));
+  },
+  signedIn: credential => {
+    debug('credential: ', credential);
+    api.saveCredential(credential);
+    api.prepareHeaders();
+    dispatch(signedIn(credential));
+  },
+  signingOut: () => {
+    dispatch(signingOut());
+  },
+  signedOut: () => {
+    api.clearCredential();
+    api.clearUserId();
+    dispatch(signedOut());
+  },
+  signInFailed: (error, info) => {
+    const now = new Date();
+    dispatch(
+      signInFailed({
+        message: 'sign in failed. see console',
+        error,
+        info,
+        time: now.toTimeString()
+      })
+    );
+  }
 });
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(MenuIcon);
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -33,79 +33,79 @@ import { host } from './utils/host';
 const debug = Debug('fabnavi:jsx:FabnaviApp');
 
 const forceSignIn = store => {
-    debug('force login');
-    const authUrl = `${host.url}/auth/github?auth_origin_url=${host.url}`;
-    window.open(authUrl);
-    const url = window.location.href;
-    debug('url: ', url);
-    if(url.includes('uid') && url.includes('client_id') && url.includes('auth_token')) {
-        const credential = {
-            accessToken: url.match(/auth_token=([a-zA-Z0-9\-_]*)/)[1],
-            uid: url.match(/uid=([a-zA-Z0-9\-_]*)/)[1],
-            client: url.match(/client_id=([a-zA-Z0-9\-_]*)/)[1]
-        };
-        api.saveCredential(credential);
-        store.dispatch(signedIn(credential));
-    }
+  debug('force login');
+  const authUrl = `${host.url}/auth/github?auth_origin_url=${host.url}`;
+  window.open(authUrl);
+  const url = window.location.href;
+  debug('url: ', url);
+  if(url.includes('uid') && url.includes('client_id') && url.includes('auth_token')) {
+    const credential = {
+      accessToken: url.match(/auth_token=([a-zA-Z0-9\-_]*)/)[1],
+      uid: url.match(/uid=([a-zA-Z0-9\-_]*)/)[1],
+      client: url.match(/client_id=([a-zA-Z0-9\-_]*)/)[1]
+    };
+    api.saveCredential(credential);
+    store.dispatch(signedIn(credential));
+  }
 };
 
 const isAuthWindow = url => {
-    return url.includes('uid') && url.includes('client_id') && url.includes('auth_token');
+  return url.includes('uid') && url.includes('client_id') && url.includes('auth_token');
 };
 
 const parseAuthInfo = url => {
-    return {
-        'Access-Token': url.match(/auth_token=([a-zA-Z0-9\-\_]*)/)[1],
-        Uid: url.match(/uid=([a-zA-Z0-9\-\_]*)/)[1],
-        Client: url.match(/client_id=([a-zA-Z0-9\-\_]*)/)[1]
-    };
+  return {
+    'Access-Token': url.match(/auth_token=([a-zA-Z0-9\-\_]*)/)[1],
+    Uid: url.match(/uid=([a-zA-Z0-9\-\_]*)/)[1],
+    Client: url.match(/client_id=([a-zA-Z0-9\-\_]*)/)[1]
+  };
 };
 
 window.api = WebAPIUtils;
 
 window.addEventListener('DOMContentLoaded', () => {
-    debug('======> Mount App');
-    const history = createMemoryHistory();
-    // const composeEnhancers = isDev ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose;
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-    const store = createStore(
-        reducers,
-        composeEnhancers(applyMiddleware(adjustor, epicsMiddleware, routerMiddleware(history)))
-    );
-    api.init(store);
-    debug(api.loadCredential());
-    // if(!api.loadCredential()) forceSignIn(store);
-    const url = window.location.href;
-    if(isAuthWindow(url)) {
-        window.opener.postMessage(JSON.stringify(parseAuthInfo(url)), window.location.origin);
-        window.close();
-        return;
-    }
-    store.dispatch(fetchProjects(0, 'all'));
-    ReactDOM.render(
-        <Provider store={store}>
-            <ConnectedRouter history={history}>
+  debug('======> Mount App');
+  const history = createMemoryHistory();
+  // const composeEnhancers = isDev ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose;
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(adjustor, epicsMiddleware, routerMiddleware(history)))
+  );
+  api.init(store);
+  debug(api.loadCredential());
+  // if(!api.loadCredential()) forceSignIn(store);
+  const url = window.location.href;
+  if(isAuthWindow(url)) {
+    window.opener.postMessage(JSON.stringify(parseAuthInfo(url)), window.location.origin);
+    window.close();
+    return;
+  }
+  store.dispatch(fetchProjects(0, 'all'));
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route component={ProjectPlayer} path="/play/:projectId" />
+          <Route component={WorkSpace} path="/workspace" />
+          <Route
+            path="/"
+            render={() => (
+              <ProjectManager>
                 <Switch>
-                    <Route component={ProjectPlayer} path="/play/:projectId" />
-                    <Route component={WorkSpace} path="/workspace" />
-                    <Route
-                        path="/"
-                        render={() => (
-                            <ProjectManager>
-                                <Switch>
-                                    <Route component={ProjectList} path="/" exact />
-                                    <Route component={ProjectList} path="/myprojects" />
-                                    <Route component={CreateProject} path="/create" />
-                                    <Route component={ProjectEditForm} path="/edit/:projectId" />
-                                    <Route component={ProjectDetail} path="/detail/:projectId" />
-                                </Switch>
-                            </ProjectManager>
-                        )}
-                    />
+                  <Route component={ProjectList} path="/" exact />
+                  <Route component={ProjectList} path="/myprojects" />
+                  <Route component={CreateProject} path="/create" />
+                  <Route component={ProjectEditForm} path="/edit/:projectId" />
+                  <Route component={ProjectDetail} path="/detail/:projectId" />
                 </Switch>
-            </ConnectedRouter>
-        </Provider>,
-        document.getElementById('app')
-    );
-    window.addEventListener('keydown', handleKeyDown(store));
+              </ProjectManager>
+            )}
+          />
+        </Switch>
+      </ConnectedRouter>
+    </Provider>,
+    document.getElementById('app')
+  );
+  window.addEventListener('keydown', handleKeyDown(store));
 });
