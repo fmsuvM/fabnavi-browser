971a81ca561781f93875aebe620098a71cf4bf4a
fmsuvM <k222.tkd.fms@gmail.com>
2018-11-05 17:42:45 +0900

    Feat: update RelatedProjects style
    横スクロールで閲覧できるようにした

--- src/renderer/stylesheets/application/ProjectShow/RelatedProjects.js
+++ src/renderer/stylesheets/application/ProjectShow/RelatedProjects.js
@@ -3,4 +3,6 @@ import styled, { css } from 'styled-components';
 export const ProjectFrame = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: center;
+  overflow-x: scroll;
 `;
