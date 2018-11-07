624691a684ac8c3f3dfb21760ef0bb10af4ef944
fmsuvM <k222.tkd.fms@gmail.com>
2018-11-07 21:40:50 +0900

    Feat: add current step mark
    現在自分がどの工程にいるかわかるようにした

--- src/renderer/components/Visualizer/ProcessVisualizer.js
+++ src/renderer/components/Visualizer/ProcessVisualizer.js
@@ -22,6 +22,7 @@ export default class ProcessVisualizer extends React.Component {
   render() {
     const project = this.props.project;
     const step = project.content.length;
+    const currentStep = this.props.index;
     return (
       <VisualizeFrame>
         <Stage width={window.innerWidth} height={window.innerHeight / 5.5}>
@@ -34,6 +35,7 @@ export default class ProcessVisualizer extends React.Component {
                   index={index}
                   step={step}
                   contents={_content}
+                  isSelected={index === currentStep}
                   onClick={this.props.handleThumbnailClick}
                 />
               );
@@ -45,12 +47,15 @@ export default class ProcessVisualizer extends React.Component {
   }
 }
 
+// step: 合計ステップ数
+// index: 現在のステップ数
 // TODO: 工程数に応じてnodeの大きさを変える
 // TODO: 工程ごとに，タグ付けできるようにする．→一旦stateか何かで保持しておくようにするか
 // TODO: 一つの工程を選択したら，同様の，または類似度の高い工程を含むコンテンツを提示する→視覚定時
 // TODO: 直列に行うか，並列に行うかを考察する．EDITで編集できるようにするかどうか．並列になった場合どうするのか．指示があったが方がいいのか
-const DrawNode = ({ index, step, contents, onClick }) => {
+const DrawNode = ({ index, step, contents, onClick, isSelected }) => {
   let nodepos;
+  let nodeColor = '#000';
   if(index === 0) {
     nodepos = 50 + index * (end / (step - 1));
   } else if(index + 1 == step) {
@@ -59,17 +64,24 @@ const DrawNode = ({ index, step, contents, onClick }) => {
     nodepos = index * (end / (step - 1));
   }
 
-  return <Circle fill="#00ff00" x={nodepos} y={100} radius={25} onClick={onClick} />;
+  if(isSelected) {
+    nodeColor = '#00ff00';
+  }
+
+  return <Circle fill={nodeColor} x={nodepos} y={100} radius={25} onClick={onClick} />;
 };
 
 ProcessVisualizer.propTypes = {
   children: PropTypes.string,
   project: PropTypes.object,
-  step: PropTypes.number
+  step: PropTypes.number,
+  index: PropTypes.number,
+  handleThumbnailClick: PropTypes.func
 };
 
 DrawNode.propTypes = {
   index: PropTypes.number,
   step: PropTypes.number,
-  contents: PropTypes.object
+  contents: PropTypes.object,
+  isSelected: PropTypes.bool
 };
