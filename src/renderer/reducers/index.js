b268be83af8ebfc299c2ee376a1aa5e69ee3ea0a
fmsuvM <k222.tkd.fms@gmail.com>
2018-12-04 02:31:51 +0900

    Feat: add analyzer reduver

--- src/renderer/reducers/index.js
+++ src/renderer/reducers/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import Debug from 'debug';
 
-
 import player from './player';
 import user from './user';
 import manager from './manager';
 import modals from './modals';
+import analyzer from './analyzer';
 import errors from './errors';
 
 const debug = Debug('fabnavi:reducer');
@@ -16,6 +16,7 @@ export default combineReducers({
   user,
   manager,
   modals,
+  analyzer,
   errors,
   router: routerReducer
 });
