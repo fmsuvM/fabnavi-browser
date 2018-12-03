94104d49de9891eafa55e163ac848ec9b5568bd5
fmsuvM <k222.tkd.fms@gmail.com>
2018-12-04 02:28:10 +0900

    Feat: add analyzer action/reducer
    物体検出とナレーション解析をおこなうためのactionとreducerを追加

---
+++ src/renderer/actions/analyzer.js
@@ -0,0 +1,41 @@
+import { createAction } from 'redux-actions';
+
+export const INITIALIZE_DATA = 'INITIALIZE_DATA';
+export const REQUEST_DETECTION = 'REQUEST_DETECTION';
+export const REQUEST_TRANSCRIPTION = 'REQUEST_TRANSCRIPTION';
+export const FETCH_RESULTS = 'FETCH_RESULTS';
+export const FETCHING_RESULTS = 'FETCHING_RESULTS';
+export const RECEIVE_DETECTION_RESULTS = 'RECEIVE_DETECTION_RESULTS';
+export const RECEIVE_TRANSCRIPTION_RESULTS = 'RECEIVE_TRANSCRIPTION_RESULTS';
+
+export const initializeData = createAction(INITIALIZE_DATA, data => {
+  return {
+    data
+  };
+});
+export const requestDetection = createAction(REQUEST_DETECTION, url => {
+  return {
+    url
+  };
+});
+export const requestTranscription = createAction(REQUEST_TRANSCRIPTION, url => {
+  return {
+    url
+  };
+});
+export const fetchResults = createAction(FETCH_RESULTS, url => {
+  return {
+    url
+  };
+});
+export const fetchingResults = createAction(FETCHING_RESULTS);
+export const receiveDetectionResults = createAction(RECEIVE_DETECTION_RESULTS, data => {
+  return {
+    data
+  };
+});
+export const receiveTranscriptionResults = createAction(RECEIVE_TRANSCRIPTION_RESULTS, data => {
+  return {
+    data
+  };
+});
---
+++ src/renderer/reducers/analyzer.js
@@ -0,0 +1,85 @@
+import { handleActions } from 'redux-actions';
+import Debug from 'debug';
+
+import {
+  INITIALIZE_DATA,
+  RECEIVE_DETECTION_RESULTS,
+  FETCHING_RESULTS,
+  RECEIVE_TRANSCRIPTION_RESULTS
+} from '../actions/analyzer';
+
+const debug = Debug('fabnavi:reducer:analyzer');
+
+const initialState = {
+  figures: [],
+  isFetching: false
+};
+
+const createFiguresInfo = figures => {
+  const format = [];
+  figures.map(figure => {
+    format.push({
+      source: figure.figure.file.url,
+      original: figure.figure.file.thumb.url,
+      detected: null,
+      detection: {},
+      transcription: {}
+    });
+  });
+  return format;
+};
+
+const addDetectionInfo = (state, detection) => {
+  const copyFigures = state;
+  const original = state[detection.index];
+  const copy = Object.assign({}, original);
+  copy.detection['detected'] = detection.detected;
+  copy.detection['unknown'] = detection.unknown;
+  copyFigures[detection.index] = copy;
+  return copyFigures;
+};
+
+const addTranscriptionInfo = (state, transcription) => {
+  const copyFigures = state;
+  const original = state[transcription.index];
+  const copy = Object.assign({}, original);
+  copy.transcription['narration'] = transcription.narration;
+  copy.transcription['words'] = transcription.words;
+  copyFigures[transcription.index] = copy;
+  return copyFigures;
+};
+
+export default handleActions(
+  {
+    [INITIALIZE_DATA]: (state, action) => {
+      const{ data } = action.payload;
+      debug('inistializing data: ', data);
+      const figures = data.content;
+      return Object.assign({}, state, {
+        figures: createFiguresInfo(figures),
+        isFetching: false
+      });
+    },
+    [FETCHING_RESULTS]: (state, action) => {
+      return Object.assign({}, state, {
+        ...state,
+        isFetching: true
+      });
+    },
+    [RECEIVE_DETECTION_RESULTS]: (state, action) => {
+      const{ data } = action.payload;
+      return Object.assign({}, state, {
+        figures: addDetectionInfo(state.figures, data),
+        isFetching: false
+      });
+    },
+    [RECEIVE_TRANSCRIPTION_RESULTS]: (state, action) => {
+      const{ data } = action.payload;
+      return {
+        figures: addTranscriptionInfo(state.figures, data),
+        isFetching: false
+      };
+    }
+  },
+  initialState
+);
