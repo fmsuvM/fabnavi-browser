66f00647f744b99b38a98b729f9af7d4f369231e
fmsuvM <k222.tkd.fms@gmail.com>
2018-12-12 14:41:11 +0900

    [WIP]: update player size

--- src/renderer/stylesheets/player/Player.js
+++ src/renderer/stylesheets/player/Player.js
@@ -1,20 +1,21 @@
 import styled from 'styled-components';
 
 export const ImagePlayer = styled.canvas`
-    display: table-cell;
-    width: 100%;
-    height: 100%;
+  display: table-cell;
+  width: 100%;
+  height: 100%;
 `;
 
 export const VideoPanel = styled.video`
-    &::-webkit-media-controls-panel {
-        display: flex !important;
-        opacity: 1 !important;
-    }
+  margin-left: -200px;
+  &::-webkit-media-controls-panel {
+    display: flex !important;
+    opacity: 1 !important;
+  }
 `;
 
 export const ImageType = styled.div`
-    font-size: 20px;
-    font-weight: 700;
-    margin-bottom: 10px;
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 10px;
 `;
